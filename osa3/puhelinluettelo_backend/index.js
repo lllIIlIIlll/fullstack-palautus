27dd4e9e996dd66ce36541dcb33ef4ddc3793766
arttu <arttu.salminen@helsinki.fi>
2024-04-06 12:06:36 +0300

    created port variable for render

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -79,6 +79,7 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
-app.listen(3001, () => {
-    console.log('server running on port 3001')
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
 })
