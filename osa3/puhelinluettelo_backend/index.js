5852e479d86ca773d088252b1aaa5a4031e39d6b
arttu <arttu.salminen@helsinki.fi>
2024-04-06 12:00:45 +0300

    puhelinluettelo step 10,11

---
+++ osa3/puhelinluettelo_backend/index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+      {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": "1"
+      },
+      {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": "2"
+      },
+      {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": "3"
+      },
+      {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": "4"
+      },
+    ]
+
+app.get('/', (req, res) => {
+    res.send('index')
+})
+
+app.get('/info', (req, res) => {
+    const date = new Date
+    res.send(`Phonebook has info for ${persons.length} people <br><br>${date}`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const person = persons.filter(person => person.id === id)[0]
+    console.log(person)
+    person ? res.json(person) : res.status(404).end()
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ 
+        error: 'name/number missing' 
+    })
+  } else if (persons.filter(person => person.name === body.name).length > 0) {
+    return res.status(400).json({ 
+        error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: String(Math.floor(Math.random() * 10000) + 1)
+  }
+  persons = persons.concat(person)
+  res.json(person)
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.listen(3001, () => {
+    console.log('server running on port 3001')
+})
---
+++ osa3/puhelinluettelo_frontend/src/App.jsx
@@ -0,0 +1,143 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import contactsService from './services/contacts'
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+  const [searched, setSearched] = useState(persons)
+  const [notification, setNotification] = useState(null)
+
+  useEffect(() => {
+    contactsService
+      .getAll()
+      .then(initialContacts => {
+        setPersons(initialContacts)
+      })
+  }, [])
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+    let filtered = persons.filter(person => person.name.toLowerCase().includes(event.target.value.toLowerCase()))
+    setSearched(filtered)
+    if (event.target.value === '') {
+      setSearched([])
+    }
+  }
+
+  const addContact = (event) => {
+    event.preventDefault()
+    let duplicate = false
+    persons.forEach(person => {
+      if (person.name === newName) {
+        duplicate = true
+      }
+    })
+    duplicate ? alert(`${newName} is already added to phonebook`) : setPersons(persons.concat({name: newName, number: newNumber}))
+    setPersons(persons.concat({name: newName, number: newNumber, id: persons.length+1}))
+
+    const contactObject = {
+      name: newName,
+      number: newNumber,
+      id: (persons.length+1).toString()
+    }
+  
+    contactsService
+      .create(contactObject)
+      .then(returnedContact => {
+        setPersons(persons.concat(returnedContact))
+        setNewName('')
+        setNewNumber('')
+        setNotification(
+          [`Added ${returnedContact.name}`, 0]
+        )
+        setTimeout(() => {
+          setNotification(null)
+        }, 3000)
+      })
+  }
+
+  const handleDeletion = (id) => {
+    if (window.confirm(`Delete ${persons.filter(person => person.id === id)[0].name}?`)) {
+      contactsService
+      .deleteById(id)
+      .then(returnedContact => {
+        setPersons(persons.filter(person => person.id !== id))
+      })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={notification}/>
+      <Filter newSearch={newSearch} handleSearchChange={handleSearchChange}/>
+      <h2>add new</h2>
+      <PersonForm 
+        addContact={addContact}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons 
+        searched={persons.filter(person => searched.length === 0 && newSearch === '' ? persons : searched.includes(person))} 
+        handleDelete={handleDeletion}
+      />
+    </div>
+  )
+
+}
+
+const Filter = ({ newSearch, handleSearchChange}) => {
+  return (
+    <div>
+      filter shown with: <input value={newSearch} onChange={handleSearchChange} />
+    </div>
+  )
+}
+
+const PersonForm = ({ addContact, newName, handleNameChange, newNumber, handleNumberChange }) => {
+  return (
+    <form onSubmit={addContact}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+  )
+}
+
+const Persons = ({ searched, handleDelete }) => {
+  return searched.map(person => <p key={person.id}>{person.name} {person.number} <button onClick={() => handleDelete(person.id)}>delete</button></p>)
+}
+
+const Notification = ({ message, type }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="success_message">
+      {message[0]}
+    </div>
+  )
+}
+
+export default App
